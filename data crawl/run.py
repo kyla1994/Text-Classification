c420125b9152a963c99cbd8f0d8c09f5e4ae67b1
kyla1994 <wangwenhui2012@126.com>
2017-12-12 22:49:42 +0800

    Add files via upload

---
+++ data crawl/run.py
@@ -0,0 +1,10 @@
+from scrapy import cmdline
+
+#cmdline.execute("scrapy crawl ifeng".split())
+#cmdline.execute("scrapy crawl mil".split())
+#cmdline.execute("scrapy crawl history".split())
+#cmdline.execute("scrapy crawl history1".split())
+# cmdline.execute("scrapy crawl tech".split())
+# cmdline.execute("scrapy crawl sports".split())
+#cmdline.execute("scrapy crawl politics".split())
+cmdline.execute("scrapy crawl mil3".split())
